8c5b2faff688fd5f5e6140cb22edd8262845b6cf
admin <nicole.georgieva18@abv.bg>
2024-07-30 16:16:52 +0300

    changing extension perspective

--- codbex-order-payment-ext/generate/CustomerPayment/customer-payment-generate.js
+++ codbex-order-payment-ext/generate/CustomerPayment/customer-payment-generate.js
@@ -2,8 +2,8 @@ const viewData = {
     id: 'customer-payment-generate',
     label: 'Generate Customer Payment',
     link: '/services/web/codbex-order-payment-ext/generate/CustomerPayment/generate-customer-payment.html',
-    perspective: 'CustomerPayment',
-    view: 'CustomerPayment',
+    perspective: 'SalesOrder',
+    view: 'SalesOrder',
     type: 'entity',
     order: 23
 };
